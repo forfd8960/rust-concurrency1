f6333fc24d214a8f452658080268559df31b1bb6
forfd8960 <forfd8960@gmail.com>
2024-12-12 09:04:41 +0800

    feat: run muultiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use rust_concurrency_programming::thread::{start_2_thread, start_thread, start_thread_with_value};
+use rust_concurrency_programming::thread::{
+    build_thread, current_thread, start_2_thread, start_multiple_threads, start_thread,
+    start_thread_with_value,
+};
 
 fn main() {
     println!("Rust Concurrency");
     start_thread();
     start_thread_with_value();
     start_2_thread();
+
+    start_multiple_threads();
+    build_thread();
+    current_thread();
 }
--- src/thread/mod.rs
+++ src/thread/mod.rs
@@ -36,3 +36,52 @@ pub fn start_2_thread() {
     h1.join().unwrap();
     h2.join().unwrap()
 }
+
+pub fn start_multiple_threads() {
+    let handles: Vec<_> = (0..10)
+        .map(|i| thread::spawn(move || println!("Hello from thread: {}", i)))
+        .collect();
+
+    handles.into_iter().for_each(|f| f.join().unwrap());
+}
+
+pub fn build_thread() {
+    let builder = thread::Builder::new()
+        .name("my thread".to_string())
+        .stack_size(32 * 1024); // 32 bytes
+
+    let handler = builder
+        .spawn(|| {
+            println!("from builder thread");
+        })
+        .unwrap();
+
+    handler.join().unwrap()
+}
+
+pub fn current_thread() {
+    let cur_thread = thread::current();
+    println!(
+        "current thread: {:?}, {:?}",
+        cur_thread.id(),
+        cur_thread.name()
+    );
+
+    let builder = thread::Builder::new()
+        .name("new thread".to_string())
+        .stack_size(32 * 1024); // 32 bytes
+
+    let handler = builder
+        .spawn(|| {
+            println!("from builder thread");
+            let cur_thread = thread::current();
+            println!(
+                "current thread: {:?}, {:?}",
+                cur_thread.id(),
+                cur_thread.name()
+            );
+        })
+        .unwrap();
+
+    handler.join().unwrap()
+}
